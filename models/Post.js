55a7077a284205c1e748b32fd4e3b8c7e3b8b9e9
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-07-22 17:22:43 -0500

    vote

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,30 @@
-const {Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 
 class Post extends Model {
-
+    static upvote(body, models) {
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            });
+        });
+    }
 }
 Post.init(
     {
@@ -15,7 +36,7 @@ Post.init(
         },
         title: {
             type: DataTypes.STRING,
-            allowNull:false
+            allowNull: false
         },
         post_url: {
             type: DataTypes.STRING,
@@ -24,7 +45,7 @@ Post.init(
                 isURL: true
             }
         },
-        user_id : {
+        user_id: {
             type: DataTypes.INTEGER,
             references: {
                 model: 'user',
