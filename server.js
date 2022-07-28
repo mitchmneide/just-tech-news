e3b8416b2c445e3277235b2be28453a043d4aeb7
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-07-28 00:55:53 -0500

    partials

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({helpers});
 const session = require('express-session');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
